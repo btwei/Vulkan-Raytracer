939c2792ec067edddf6063e444c411080fb8ec8e
Benjamin Wei <ben.stwei@gmail.com>
2025-12-12 12:06:39 -0500

    Add thread safe window resizing and dimension checking methods

--- engine/Window.cpp
+++ engine/Window.cpp
@@ -27,7 +27,7 @@ Window::~Window() {
 void Window::open(const std::string& windowName, int width, int height) {
     if(_window != nullptr) throw std::runtime_error("Each window object may only open one window!");
 
-    _window = SDL_CreateWindow(windowName.c_str(), width, height, SDL_WINDOW_VULKAN);
+    _window = SDL_CreateWindow(windowName.c_str(), width, height, SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
     if(_window == NULL) throw std::runtime_error("Failed to create SDL3 Window!");
 
     _windowID = SDL_GetWindowID(_window);
@@ -51,12 +51,27 @@ bool Window::handleEvents() {
     SDL_Event e;
     while(SDL_PollEvent(&e)) {
         if(e.type == SDL_EVENT_QUIT) _shouldClose = true;
+        if(e.type == SDL_EVENT_WINDOW_RESIZED) {
+            {
+                std::lock_guard<std::mutex> guard(mutex);
+                SDL_GetWindowSize(_window, &_width, &_height);
+                SDL_GetWindowSizeInPixels(_window, &_framebufferWidth, &_framebufferHeight);
+                _wasResized = true;
+            }
+        }
 
         // TODO: handle events and update Window class's internal state
         // will do this later as I determine what states are needed / how to handle dearimgui integration--specifically capturing inputs
     }
 
     return true;
+}
+
+bool Window::getWasResized() {
+    std::lock_guard<std::mutex> guard(mutex);
+    bool resized = _wasResized;
+    if(resized == true) _wasResized = false;
+    return resized;
 }
 
 }
--- engine/Window.hpp
+++ engine/Window.hpp
@@ -1,6 +1,7 @@
 #ifndef VKRT_WINDOW_HPP
 #define VKRT_WINDOW_HPP
 
+#include <mutex>
 #include <string>
 
 #include "SDL3/SDL.h"
@@ -15,6 +16,9 @@ namespace vkrt {
  * @note Window functions should be called from the main thread!
  */
 class Window {
+private:
+    std::mutex mutex;
+
 public:
     Window();
     Window(const std::string& windowName, int width, int height);
@@ -31,11 +35,18 @@ public:
     bool handleEvents();
     bool getShouldClose() const { return _shouldClose; };
 
+    /**
+     * @brief Gets _wasResized and expects caller to handle window resize. Sets _wasResized to false after calling.
+     * 
+     * @warning This function should only be called from the Renderer class. 
+     */
+    bool getWasResized();
+
     SDL_Window* getWindow() const { return _window; };
-    int getWidth() const { return _width; };
-    int getHeight() const { return _height; };
-    int getFramebufferWidth() const { return _framebufferWidth; };
-    int getFramebufferHeight() const { return _framebufferHeight; };
+    int getWidth() { std::lock_guard<std::mutex> guard(mutex); return _width; };
+    int getHeight() { std::lock_guard<std::mutex> guard(mutex); return _height; };
+    int getFramebufferWidth() { std::lock_guard<std::mutex> guard(mutex); return _framebufferWidth; };
+    int getFramebufferHeight() { std::lock_guard<std::mutex> guard(mutex); return _framebufferHeight; };
 
 private:
     static int _instanceCount;
@@ -48,6 +59,7 @@ private:
     int _framebufferHeight = 0;
 
     bool _shouldClose = false;
+    bool _wasResized = false;
 };
 
 } // namespace vkrt
