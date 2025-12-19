85415604d9279143abf100ddc508e65b00f9618a
Benjamin Wei <ben.stwei@gmail.com>
2025-12-19 14:49:11 -0600

    Add VMA to engine CMakeLists and add implementation file

---
+++ engine/VmaUsage.cpp
@@ -0,0 +1,6 @@
+//VMA implementation file
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Weverything"
+#define VMA_IMPLEMENTATION
+#include <vk_mem_alloc.h>
+#pragma clang diagnostic pop
