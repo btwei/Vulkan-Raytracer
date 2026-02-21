3937cc445674ef09aa85a41ca2829b387b24f429
Benjamin Wei <ben.stwei@gmail.com>
2026-02-20 22:58:14 -0500

    Add empty/skeleton VulkanSwapchain class

---
+++ engine/src/rendering/VulkanSwapchain.cpp
@@ -0,0 +1,25 @@
+#include "VulkanSwapchain.hpp"
+
+namespace vkrt {
+
+VulkanSwapchain::VulkanSwapchain() {
+
+}
+
+VulkanSwapchain::~VulkanSwapchain() {
+
+}
+
+void VulkanSwapchain::resize() {
+
+}
+
+void VulkanSwapchain::createSwapchain() {
+
+}
+
+void VulkanSwapchain::destroySwapchain() {
+
+}
+
+}
