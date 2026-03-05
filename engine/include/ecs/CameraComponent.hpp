7dd5eb5bbc80b21fc92b0c06128891ef68aeb1c5
Benjamin Wei <ben.stwei@gmail.com>
2026-03-05 05:13:03 -0500

    Remove aspectRatio from CameraComponent; aspectRatio is owned by the window / renderer

--- engine/include/ecs/CameraComponent.hpp
+++ engine/include/ecs/CameraComponent.hpp
@@ -10,12 +10,11 @@ namespace vkrt {
 class CameraComponent : public Component {
 public:
     float fov = 90.0f;
-    float aspectRatio = 16.0f / 9.0f; // Based on the window dimensions - width / height
     float nearPlane = 0.1f; // Tmin
     float farPlane = 1000.0f; // Tmax
 
-    glm::mat4 viewMatrix = glm::mat4(1.0f);
-    glm::mat4 projectionMatrix = glm::mat4(1.0f);
+    glm::mat4 viewMatrix = glm::mat4(1.0f); // Updated by the engine Renderer system -- based on transform component
+    glm::mat4 projectionMatrix = glm::mat4(1.0f); // Updated by the engine Renderer system -- based on camera component
     bool isDirty = true; // mark dirty when changed, rendering system will mark clean
 };
 
