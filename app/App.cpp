d8468970cf184cc14b48593438603a6195b2f4d8
Benjamin Wei <ben.stwei@gmail.com>
2026-03-04 22:12:08 -0500

    Add comments to App class

--- app/App.cpp
+++ app/App.cpp
@@ -13,21 +13,28 @@ App::App(int argc, char* argv[]) :
 void App::run() {
     _engine.init("Vulkan Raytracer", 800, 600);
 
-    // TODO: Add application logic here-- cameras, object loading, etc.
+    // Import a GLTF file into handles
     vkrt::ImportResult statueImport = _engine.getAssetManager()->importAsset("../assets/horse_statue_01_8k/horse_statue_01_8k.gltf");
     vkrt::AssetHandle<vkrt::ModelAsset> statueHandle = statueImport.modelHandles[0];
 
+    // Create a new statue entity
     vkrt::Entity* statue = _engine.getEntityManager()->createNewEntity("statueEntity");
     statue->addComponent<vkrt::MeshComponent>(statueHandle);
     statue->addComponent<vkrt::TransformComponent>();
     
+    // Create a camera entity
     vkrt::Entity* camera = _engine.getEntityManager()->createNewEntity("cameraEntity");
     camera->addComponent<vkrt::CameraComponent>();
     vkrt::TransformComponent* cameraTransform = camera->addComponent<vkrt::TransformComponent>();
     cameraTransform->position = glm::vec3(0.0f, 0.0f, 10.0f);
 
+    // Register systems in order
     std::shared_ptr<vkrt::System> testSystem = std::make_shared<TestSystem>();
     _engine.getEntityManager()->registerSystem(testSystem);
 
+    std::shared_ptr<vkrt::System> cameraSystem = std::make_shared<CameraSystem>();
+    _engine.getEntityManager()->registerSystem(cameraSystem);
+
+    // Run
     _engine.run();
 }
