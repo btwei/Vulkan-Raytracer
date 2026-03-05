1c55c5b27c3cec563951cf8d991b3d794c9f455c
Benjamin Wei <ben.stwei@gmail.com>
2026-03-04 22:09:12 -0500

    Update demo CameraSystem with simple test logic

--- app/CameraSystem.cpp
+++ app/CameraSystem.cpp
@@ -1,5 +1,35 @@
 #include "CameraSystem.hpp"
 
-void CameraSystem::update(std::vector<std::unique_ptr<vkrt::Entity>>& entityList, const vkrt::GlobalSingletons globalSingletons) {
-    
+#include "CameraComponent.hpp"
+#include "TransformComponent.hpp"
+
+void CameraSystem::update(std::vector<std::unique_ptr<vkrt::Entity>>& entityList, vkrt::GlobalSingletons globalSingletons) {
+    if(globalSingletons.activeCameraEntity != nullptr) {
+        bool active = globalSingletons.activeCameraEntity->isActive();
+        vkrt::CameraComponent* cameraComponent = globalSingletons.activeCameraEntity->getComponent<vkrt::CameraComponent>();
+        vkrt::TransformComponent* transformComponent = globalSingletons.activeCameraEntity->getComponent<vkrt::TransformComponent>();
+
+        if(active && cameraComponent && transformComponent) {
+            // Use input state to move the camera entity's transform 
+            if(globalSingletons.inputState.keyboardState.keys[vkrt::VKRT_W_KEY].down) {
+                transformComponent->position += glm::vec3(0.0, 0.0, 0.01);
+                cameraComponent->isDirty = true;
+            }
+
+            if(globalSingletons.inputState.keyboardState.keys[vkrt::VKRT_S_KEY].down) {
+                transformComponent->position -= glm::vec3(0.0, 0.0, 0.01);
+                cameraComponent->isDirty = true;
+            }
+
+            if(globalSingletons.inputState.keyboardState.keys[vkrt::VKRT_A_KEY].down) {
+                transformComponent->position -= glm::vec3(0.01, 0.0, 0.0);
+                cameraComponent->isDirty = true;
+            }
+
+            if(globalSingletons.inputState.keyboardState.keys[vkrt::VKRT_D_KEY].down) {
+                transformComponent->position += glm::vec3(0.01, 0.0, 0.01);
+                cameraComponent->isDirty = true;
+            }
+        }
+    }
 }
