06dcc8b9f84994340f45f0f6e251342495a31ffa
Benjamin Wei <ben.stwei@gmail.com>
2026-03-23 19:40:37 -0400

    Add FrameManager class and partial implementation

---
+++ engine/include/rendering/FrameManager.hpp
@@ -0,0 +1,68 @@
+#ifndef VKRT_FRAMEMANAGER_HPP
+#define VKRT_FRAMEMANAGER_HPP
+
+#include "DeletionQueue.hpp"
+#include "DescriptorManager.hpp"
+#include "ResourceManager.hpp"
+#include "VulkanContext.hpp"
+#include "VulkanDescriptors.hpp"
+#include "VulkanTypes.hpp"
+
+namespace vkrt {
+    
+static const int NUM_FRAMES_IN_FLIGHT = 2;
+
+struct FrameData {
+    VkCommandPool commandPool;
+    VkCommandBuffer mainCommandBuffer;
+
+    VkFence renderFence;
+    VkSemaphore acquireToRenderSemaphore;
+
+    Descriptor1 descriptorSet1;
+
+    AllocatedImage drawImage;
+    TlasResources tlasResources;
+
+    DeletionQueue deletionQueue;
+};
+
+/**
+ * @class FrameManager
+ * @brief Handles per frame data in the Renderer class
+ */
+class FrameManager {
+public:
+    FrameManager(VulkanContext& ctx, ResourceManager& resourceManager, DescriptorManager& descriptorManager, VkExtent2D swapchainExtent);
+    ~FrameManager();
+
+    uint64_t frameCount = 0;
+    FrameData frameData[NUM_FRAMES_IN_FLIGHT];
+
+    void resizeDrawImages(VkExtent2D swapchainExtent);
+    void cleanupPerFrame();
+
+    uint64_t getFrameNumber() { return frameCount; }
+
+    FrameData& getCurrentFrame() { return frameData[frameCount % NUM_FRAMES_IN_FLIGHT]; }
+    FrameData& getPreviousFrame() { return frameData[(frameCount - 1) % NUM_FRAMES_IN_FLIGHT]; }
+    FrameData& getNextFrame() { return frameData[(frameCount + 1) % NUM_FRAMES_IN_FLIGHT]; }
+
+private:
+    VulkanContext& _vulkanContext;
+    VkDevice _device;
+    uint32_t _graphicsQueueFamilyIndex;
+    ResourceManager& _resourceManager;
+    DescriptorManager& _descriptorManager;
+
+    void initCommandResources();
+    void initSyncResources();
+    void initDrawImages(VkExtent2D swapchainExtent);
+    void initDescriptorSets();
+    void initTlasResources();
+
+};
+
+} // namespace vkrt
+
+#endif // VKRT_FRAMEMANAGER_HPP
---
+++ engine/src/rendering/FrameManager.cpp
@@ -0,0 +1,100 @@
+#include "FrameManager.hpp"
+
+#include "VulkanInitializers.hpp"
+#include "VulkanUtils.hpp"
+
+namespace vkrt {
+    
+FrameManager::FrameManager(VulkanContext& ctx, ResourceManager& resourceManager, DescriptorManager& descriptorManager, VkExtent2D swapchainExtent)
+: _vulkanContext(ctx)
+, _device(ctx.device)
+, _graphicsQueueFamilyIndex(ctx.graphicsQueueFamilyIndex)
+, _resourceManager(resourceManager) 
+, _descriptorManager(descriptorManager) {
+    initCommandResources();
+    initSyncResources();
+    initDrawImages(swapchainExtent);
+    initDescriptorSets();
+    initTlasResources();
+}
+
+FrameManager::~FrameManager() {
+    // Cleanup frameData
+    for(auto& data : frameData) {
+        data.deletionQueue.flushQueue();
+
+        _resourceManager.destroyTlas(data.tlasResources);
+
+        // Descriptor sets are destroyed when the pool is destroyed..
+
+        _resourceManager.destroyImage(data.drawImage);
+
+        vkDestroyFence(_device, data.renderFence, nullptr);
+        vkDestroySemaphore(_device, data.acquireToRenderSemaphore, nullptr);
+
+        vkDestroyCommandPool(_device, data.commandPool, nullptr);
+    }
+}
+
+void FrameManager::resizeDrawImages(VkExtent2D swapchainExtent) {
+
+}
+
+void FrameManager::cleanupPerFrame() {
+    
+}
+
+void FrameManager::initCommandResources() {
+    // Initialize per frame command resources
+    for(auto& data : frameData) {
+        VkCommandPoolCreateInfo poolInfo = init::defaultCommandPoolInfo(_graphicsQueueFamilyIndex, VK_COMMAND_POOL_CREATE_TRANSIENT_BIT |
+                                                                                                   VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
+        if(vkCreateCommandPool(_device, &poolInfo, nullptr, &data.commandPool) != VK_SUCCESS) throw std::runtime_error("Failed to create Vulkan command pool!");
+        
+        VkCommandBufferAllocateInfo allocInfo = init::defaultCommandBufferAllocateInfo(data.commandPool);
+        vkAllocateCommandBuffers(_device, &allocInfo, &data.mainCommandBuffer);
+    }
+}
+
+void FrameManager::initSyncResources() {
+    // Initialize per frame semaphores and fences
+    for(auto& data : frameData) {
+        VkSemaphoreCreateInfo semaphoreInfo = init::defaultSemaphoreInfo();
+        VK_REQUIRE_SUCCESS(vkCreateSemaphore(_device, &semaphoreInfo, nullptr, &data.acquireToRenderSemaphore));
+
+        VkFenceCreateInfo fenceInfo = init::defaultFenceInfo(VK_FENCE_CREATE_SIGNALED_BIT);
+        VK_REQUIRE_SUCCESS(vkCreateFence(_device, &fenceInfo, nullptr, &data.renderFence));
+    }
+}
+
+void FrameManager::initDrawImages(VkExtent2D swapchainExtent) {
+    for(FrameData& data : frameData) {
+        data.drawImage = _resourceManager.createImage(VkExtent3D(swapchainExtent.width, swapchainExtent.height, 1),
+                                                       VK_FORMAT_R16G16B16A16_SFLOAT,
+                                                       VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT);
+        
+        // Transition the draw image to general for use with raytracing pipelines
+        _vulkanContext.immediateSubmit([&](VkCommandBuffer cmdBuf){
+            utils::defaultImageTransition(cmdBuf, data.drawImage.image,
+                                          VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_GENERAL,
+                                          0, VK_ACCESS_SHADER_READ_BIT | VK_ACCESS_SHADER_WRITE_BIT,
+                                          VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_RAY_TRACING_SHADER_BIT_KHR,
+                                          1);
+        });
+    }
+}
+
+void FrameManager::initDescriptorSets() {
+    for(FrameData& data : frameData) {
+        data.descriptorSet1 = _descriptorManager.allocateLayout1();
+    }
+}
+
+void FrameManager::initTlasResources() {
+    for(FrameData& data : frameData) {
+        std::vector<BlasInstance> emptyTlas;
+        data.tlasResources = _resourceManager.buildTlas(emptyTlas);
+    }
+}
+
+} // namespace vkrt
